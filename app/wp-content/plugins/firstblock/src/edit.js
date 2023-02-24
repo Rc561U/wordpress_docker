e5f3c6b5f05c542c30a5c1545d7e9dcbbc71d1ea
“Rc561U” <hugeanton29011994@gmail.com>
2023-02-24 11:43:18 +0100

    refactor: return to default condition

--- app/wp-content/plugins/firstblock/src/edit.js
+++
@@ -1,6 +0,0 @@
-import {useBlockProps} from "@wordpress/block-editor";
-
-export default function edit() {
-    const blockProps = useBlockProps()
-    return <p {...blockProps}>Edit jsx from separate files</p>
-}
--- app/wp-content/plugins/wfmpanel/admin/js/wfmpanel-admin.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function ($){
-    $( "#accordion" ).accordion({
-        collapsible:true,
-        active:false,
-    });
-});
-
